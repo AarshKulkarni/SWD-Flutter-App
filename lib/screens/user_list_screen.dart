30fe18329b19694e3bba1f005c0b39b214e0b27c
Caproxx_119 <codelearner56@gmail.com>
2023-05-14 12:17:07 +0530

    Added UsersScreen

--- lib/screens/user_list_screen.dart
+++ lib/screens/user_list_screen.dart
@@ -15,55 +15,61 @@ class UserListScreen extends StatefulWidget {
 class _UserListScreenState extends State<UserListScreen> {
   List<ReqUser> reqUsers = [];
 
-  Future fetchData() async {
-    final response = await http.get(
-        Uri.parse('https://reqres.in/api/users?page=2'),
-        headers: {'Content-Type': 'application/json'});
+  Future<List<ReqUser>> fetchData() async {
+    final response =
+        await http.get(Uri.parse('https://reqres.in/api/users?page=2'));
+    var fetchedData = json.decode(response.body);
     if (response.statusCode == 200) {
-      final resData = Map<String, Object>.from(json.decode(response.body));
-      final data = resData['data'];
-      final userMap = List<Map>.from(data as List<dynamic>);
-      final users = userMap.map((user) {
-        return ReqUser(
-            id: user['id'] as int,
-            email: user['email'] as String,
-            first_name: user['first_name'] as String,
-            last_name: user['last_name'] as String,
-            avatar: user['avatar']) as String;
-      }).toList();
-
+      var data = fetchedData['data'];
+      for (Map i in data) {
+        ReqUser user = ReqUser(
+            id: i['id'],
+            email: i['email'],
+            first_name: i['first_name'],
+            last_name: i['last_name'],
+            avatar: i['avatar']);
+        reqUsers.add(user);
+      }
+      return reqUsers;
     } else {
-      throw Exception('Failed to fetch users');
+      return reqUsers;
     }
-  }
-//Doesnt work why??
-  @override
-  void initState() {
-    super.initState();
-    fetchData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SWD-App'),
-      ),
-      body: ListView.builder(
-        itemCount: reqUsers.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundImage: NetworkImage(reqUsers[index].avatar),
-              ),
-              title: Text(
-                  '${reqUsers[index].first_name} ${reqUsers[index].last_name}'),
-              subtitle: Text(reqUsers[index].email),
-            ),
-          );
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('SWD-App'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: FutureBuilder(
+                  future: fetchData(),
+                  builder: (context, AsyncSnapshot<List<ReqUser>> snapshot) {
+                    return ListView.separated(
+                        separatorBuilder: (context, index) {
+                          return const Divider(
+                            thickness: 2,
+                          );
+                        },
+                        itemCount: reqUsers.length,
+                        itemBuilder: ((context, index) {
+                          return ListTile(
+                            visualDensity: const VisualDensity(vertical: 4),
+                            leading: CircleAvatar(
+                              maxRadius: 25,
+                              backgroundImage:
+                                  NetworkImage(snapshot.data![index].avatar),
+                            ),
+                            title: Text(
+                                snapshot.data![index].first_name.toString()),
+                          );
+                        }));
+                  }),
+            )
+          ],
+        ));
   }
 }
